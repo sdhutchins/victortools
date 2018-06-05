b792cce165cd5a37d110ff276d51a36bdbdd19f1
sdhutchins <ms.s.hutchins@gmail.com>
2018-06-04 20:06:01 -0500

    Set up R package.

---
+++ R/parse.R
@@ -0,0 +1,13 @@
+
+#' @title Parse File
+#'
+#' @description Parse the input file
+#'
+#' @param input_file DESCRIPTION.
+#' @param experiment_name DESCRIPTION.
+#'
+#' @return RETURN_DESCRIPTION
+#' @export
+parse_file <- function(input_file, experiment_name) {
+
+}
