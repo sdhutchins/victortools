dd549b34be97f4ffa482dc689afad4c5d1f64f44
sdhutchins <ms.s.hutchins@gmail.com>
2018-06-06 19:48:08 -0500

    Added variables for columns for different experiment types.

--- R/vars.R
+++ R/vars.R
@@ -1,3 +1,20 @@
-drug_concentrations_1 <- c("","")
+#' @export
+drug_cols_1 <- c("","")
 
-drug_concentrations_2 <- c("","")
+#' @export
+drug_cols_2 <- c("","")
+
+#' @export
+drug_cols_3 <- c("","")
+
+#' @export
+sre_cols <- c("Control", "10% FBS", "100 ng/ul EGF", "10% FBS + 100 ng/ul EGF")
+
+#' @export
+cre_cols <- c("Control", "10 uM Forskolin")
+
+#' @export
+ap1_cols <- c("Control", "10 ng/ul PMA")
+
+#' @export
+nfkb_cols <- c("Control", "10 ng/ul TNFa")
