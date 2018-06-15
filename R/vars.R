84fbaa29cbe13b06bbf53ae7306790820e59ad3c
sdhutchins <ms.s.hutchins@gmail.com>
2018-06-14 20:17:17 -0500

    Updated drug_cols_1 variable.

--- R/vars.R
+++ R/vars.R
@@ -1,11 +1,11 @@
 #' @export
-drug_cols_1 <- c("","")
+drug_cols_1 <- c("-6", "-6.5", "-7", "-7.25", "-7.5", "-8", "-8.25", "-8.5", "-9", "-9.25", "-9.5", "-10")
 
 #' @export
-drug_cols_2 <- c("","")
+drug_cols_2 <- c("", "")
 
 #' @export
-drug_cols_3 <- c("","")
+drug_cols_3 <- c("", "")
 
 #' @export
 sre_cols <- c("Control", "10% FBS", "100 ng/ul EGF", "10% FBS + 100 ng/ul EGF")
