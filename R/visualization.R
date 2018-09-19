15743478c73443212e4243c27f37ee73be4ef352
sdhutchins <ms.s.hutchins@gmail.com>
2018-09-19 12:15:59 -0500

    Add a visualization.R script.

---
+++ R/visualization.R
@@ -0,0 +1,9 @@
+#' @title Expression Barplot
+#'
+#' @description A barplot of expression data.
+#'
+#' @param assay_data The assay data frame.
+#'
+#' @export
+expression_barplot <- function(assay_data) {
+}
